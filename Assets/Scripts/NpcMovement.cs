5cfbf16aa3b90f1ccc47515c086ac9f4d91bc652
Edward <theed083@gmail.com>
2023-03-16 14:43:14 +0000

    added npc movement and desk

---
+++ Assets/Scripts/NpcMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMovement : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float moveSpeed = 2f;
+
+    private int currentWaypointIndex = 0;
+
+    void Update()
+    {
+        if (waypoints.Length > 0)
+        {
+            // Move towards the current waypoint
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].position, moveSpeed * Time.deltaTime);
+
+            // Check if the NPC has reached the current waypoint
+            if (Vector2.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f)
+            {
+                // Move to the next waypoint
+                currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            }
+        }
+    }
+}
--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PickupController : MonoBehaviour
 {
     [Header("Pickup Settings")]
@@ -21,16 +20,17 @@ public class PickupController : MonoBehaviour
             if (heldObj == null)
             {
                 RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+                {
+                    PickupObject(hit.transform.gameObject);
+                }
+            }
+            else
             {
-                PickupObject(hit.transform.gameObject);
+                DropObject();
             }
         }
-        }
-        else
-        {
-            DropObject();
-        }
+
         if (heldObj != null)
         {
             MoveObject();
@@ -39,35 +39,32 @@ public class PickupController : MonoBehaviour
 
     void MoveObject()
     {
-        if(Vector3.Distance(heldObj.transform.position, holdArea.position) > 0.1f)
+        if (Vector3.Distance(heldObj.transform.position, holdArea.position) > 0.1f)
         {
             Vector3 moveDirection = (holdArea.position - heldObj.transform.position);
-            heldObjRB.AddForce(moveDirection * pickupForce);
+            heldObjRB.AddForce(moveDirection.normalized * pickupForce);
         }
     }
 
-
-     void PickupObject(GameObject pickObj)
+    void PickupObject(GameObject pickObj)
+    {
+        if (pickObj.GetComponent<Rigidbody>())
         {
-            if (pickObj.GetComponent<Rigidbody>())
-            {
-                heldObjRB = pickObj.GetComponent<Rigidbody>();
-                heldObjRB.useGravity = false;
-                    heldObjRB.drag = 10;
-                    heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
+            heldObjRB = pickObj.GetComponent<Rigidbody>();
+            heldObjRB.useGravity = false;
+            heldObjRB.drag = 10;
+            heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
 
             heldObjRB.transform.parent = holdArea;
             heldObj = pickObj;
-
-            }
         }
+    }
 
     void DropObject()
     {
         heldObjRB.useGravity = true;
-                    heldObjRB.drag = 1;
-                    heldObjRB.constraints = RigidbodyConstraints.None;
-
+        heldObjRB.drag = 1;
+        heldObjRB.constraints = RigidbodyConstraints.None;
         heldObj.transform.parent = null;
         heldObj = null;
     }
