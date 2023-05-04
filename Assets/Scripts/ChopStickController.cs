f0b7614ddf0c400ba62f60bf62a14838dc254a34
Dylan Hughes <dyl.hughes@me.com>
2023-05-05 00:58:52 +0100

    Chopstick Controls In New Scene + Adjustments to controls.
    Issue to be solved: Rigidbody has been added and the chopsticks collide with objects, but not in the way desired/intended.

--- Assets/Scripts/ChopStickController.cs
+++ Assets/Scripts/ChopStickController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChopStickController : MonoBehaviour
@@ -10,6 +11,8 @@ public class ChopStickController : MonoBehaviour
 
     public float movementSpeedFactor = 5f;
     public float rotationSpeedFactor = 50f;
+    public float anglingSpeedFactorX = 10f;
+    public float anglingSpeedFactorY = -10f;
 
     private static int pinchLimit = 80;
     private int pinchCounter = 0;
@@ -23,7 +26,7 @@ public class ChopStickController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movements 1 & 2
+        // Movements 1 & 2 (Pinching)
         if (Input.GetMouseButton(0) && pinchCounter > -pinchLimit)
         {
             pinchCounter -= 1;
@@ -39,7 +42,7 @@ public class ChopStickController : MonoBehaviour
         }
 
 
-        // Movements 3 & 4
+        // Movements 3 & 4 (Rotation)
         if (Input.GetKey(KeyCode.E))
             hand.transform.Rotate(Vector3.forward * rotationSpeedFactor * Time.deltaTime);
 
@@ -47,28 +50,34 @@ public class ChopStickController : MonoBehaviour
             hand.transform.Rotate(Vector3.back * rotationSpeedFactor * Time.deltaTime);
 
 
-        // Movements 5 & 6
-        if (Input.GetKey(KeyCode.D))
-            hand.transform.Rotate(Vector3.up * rotationSpeedFactor * Time.deltaTime);
+        // Movements 5 & 6 (Turn Right or Left)
+       // if (Input.GetKey(KeyCode.D))
+           // hand.transform.Rotate(Vector3.up * rotationSpeedFactor * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.A))
-            hand.transform.Rotate(Vector3.down * rotationSpeedFactor * Time.deltaTime);
-
-
-        // Movements 7 & 9
-        if (Input.GetKey(KeyCode.S))
-            hand.transform.Rotate(Vector3.right * rotationSpeedFactor * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.W))
-            hand.transform.Rotate(Vector3.left * rotationSpeedFactor * Time.deltaTime);
+        // if (Input.GetKey(KeyCode.A))
+           // hand.transform.Rotate(Vector3.down * rotationSpeedFactor * Time.deltaTime);
 
 
-        // Movements 10 & 11
+        // Movements 7 & 9 (Turn down or up)
+       // if (Input.GetKey(KeyCode.S))
+          //  hand.transform.Rotate(Vector3.right * rotationSpeedFactor * Time.deltaTime);
+
+       // if (Input.GetKey(KeyCode.W))
+           // hand.transform.Rotate(Vector3.left * rotationSpeedFactor * Time.deltaTime);
+
+        // This should simplify the currently ignored movements above (will remove in future version)
+
+        float h = anglingSpeedFactorX * Input.GetAxis("Mouse X");
+        float v = anglingSpeedFactorY * Input.GetAxis("Mouse Y");
+        if (Input.GetKey(KeyCode.P))
+            transform.Rotate(v, h, 0);
+
+        // Movements 10 & 11 (Move backwards or forwards)
         if (Input.GetKey(KeyCode.F))
-            hand.transform.position += Vector3.back * movementSpeedFactor * Time.deltaTime;
+            hand.transform.position -= transform.forward * Time.deltaTime * movementSpeedFactor;
 
         if (Input.GetKey(KeyCode.R))
-            hand.transform.position += Vector3.forward * movementSpeedFactor * Time.deltaTime;
+            hand.transform.position += transform.forward * Time.deltaTime * movementSpeedFactor;
 
     }
 }
