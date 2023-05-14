fa371c42ae4063e62ed804f37bf1bc4218b4ad58
Dylan Hughes <dyl.hughes@me.com>
2023-05-14 01:21:29 +0100

    Chopsticks Should Interact With Objects Set-Up correctly just fine!

--- Assets/Scripts/ChopstickBehaviour.cs
+++ Assets/Scripts/ChopstickBehaviour.cs
@@ -29,23 +29,25 @@ public class ChopstickBehaviour : MonoBehaviour
                 triggersParent.transform.parent = hand.gameObject.transform;
             }
         }
-        //triggersParent.transform.parent = hand.gameObject.transform;
-
         
+    }
+    void OnTriggerExit(Collider other)
+    {
+        //other.transform.parent = hand.gameObject.transform;
 
-        //else if (triggersParent.gameObject.CompareTag("Interactable"))
-       // {
-            
-       // }
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            other.transform.parent = null;
+        }
 
-        //if (triggersParent.gameObject.CompareTag("inHand"))
-        //{
-        //  Debug.Log("I am held already");
-        //}
-        //else
-        //{
-        //triggersParent.gameObject.tag = "inHand";
+        else
+        {
+            GameObject triggersParent = other.gameObject.transform.parent.gameObject;
+            if (triggersParent.gameObject.CompareTag("Interactable"))
+            {
+                triggersParent.transform.parent = null;
+            }
+        }
 
-            //}
     }
 }
